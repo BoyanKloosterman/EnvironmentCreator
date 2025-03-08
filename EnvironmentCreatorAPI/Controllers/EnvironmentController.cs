7e9232d15356d20d4254d483954263e066aaf773
Boyan Kloosterman <kloostermanboyan@gmail.com>
2025-03-08 22:37:03 +0100

    Revert "changed reponse & deletion"
    This reverts commit 54abb60db757009d0538be3012c9385f404f4ef8.

--- EnvironmentCreatorAPI/Controllers/EnvironmentController.cs
+++ EnvironmentCreatorAPI/Controllers/EnvironmentController.cs
@@ -118,7 +118,7 @@ namespace EnvironmentCreatorAPI.Controllers
 
             await _context.SaveChangesAsync();
 
-            return Ok("Success");
+            return NoContent();
         }
     }
 }
--- EnvironmentCreatorAPI/Controllers/ObjectController.cs
+++ EnvironmentCreatorAPI/Controllers/ObjectController.cs
@@ -133,7 +133,7 @@ namespace EnvironmentCreatorAPI.Controllers
             _context.Objects.Remove(object2D);
             await _context.SaveChangesAsync();
 
-            return Ok("Success");
+            return NoContent(); // No content response indicates successful deletion
         }
 
     }
